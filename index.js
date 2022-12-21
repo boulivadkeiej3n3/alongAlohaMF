29b5ab61e5fd09358b0fcdbae3bb6f0aca4122bb
boulivadkeiej3n3 <119795836+boulivadkeiej3n3@users.noreply.github.com>
2022-12-21 15:30:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+const Puppeteer =  require(`puppeteer`);
 const HTTP = require("http");
-HTTP.createServer((req,res)=>{
-res.end(`hello world`);
+const Browser;
+const Page;
+
+async function main(){
+Browser = await Puppeteer.launch({args:["--no-sandbox"]})
+Page    = await Browser.newPage();
+}
+HTTP.createServer(async (req,res)=>{
+if(Page){
+  await Page.goto(`https://google.com`);
+  res.end(`${await Page.content()}`);
+}
+  res.end(`hello world`);
+
 }).listen(process.env.PORT,()=>console.log(`server is listening`))
+main();
